eb8e49d609905aa45f988ac4e21309ac4dd4e6b7
varanasi <593486@cognizant.com>
2017-04-04 14:37:29 +0530

    make it better

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent }  from './events-app.component';
-import { EventsListComponent} from   './events/events-list.component'
-import { EventThumbnailComponent} from   './events/event-thumbnail.component'
+
 
 
 @NgModule({
-    declarations: [ EventsAppComponent,
-     EventsListComponent,
-     EventThumbnailComponent
-     ],
+    declarations: [ EventsAppComponent],
     imports: [ BrowserModule],
     providers: [],
     bootstrap: [ EventsAppComponent ]
--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core'
 @Component({
 
 selector : 'events-app',
-template : '<events-list></events-list>'
+template : '<h2>Hi LP Team 5</h2>'
 
 })
 
